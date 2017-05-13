fdf698061d1f3ced85f8a89dd167b814030550cc
林宁宁 <763465697@qq.com>
2017-05-13 09:40:00 +0800

    init

---
+++ SecondsTimer/SecondsTimer/vc/YHHomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  YHHomeViewController.swift
+//  SecondsTimer
+//
+//  Created by 林宁宁 on 2017/5/13.
+//  Copyright © 2017年 林宁宁. All rights reserved.
+//
+
+import UIKit
+
+class YHHomeViewController: YHBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.title = "秒表"
+        self.view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
